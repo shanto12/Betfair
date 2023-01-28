5910811bdc1baa79ebfaa7f3d6a04fae9f92650e
Shanto Mathew <shanto12@gmail.com>
2023-01-29 07:59:59 +1100

    first push

--- CONSTS.py
+++
@@ -1,9 +0,0 @@
-APPLICATION_ID = "115742"
-
-APPLICATION_KEY_1 = "BBB1TZT0ftZmoWsl"
-
-APPLICATION_KEY_2 = "Wfh6RQ0W8Y1iDKvX"
-CERT_FILE_LOCAITON = "C:/Program Files/OpenSSL-Win64/bin/cert.pem"
-
-USER_NAME = "shanto12@gmail.com"
-PASSWORD = ""
